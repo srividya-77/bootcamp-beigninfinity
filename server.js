2a995a83912b7bc9dc104036897dc816ef5caec1
morampudimahi <morampudimahi@gmail.com>
2026-07-03 13:41:50 +0530

    feat:deploying it

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5003;
+const port = process.env.PORT || 5003;
 // webserver
 app.use(express.static("frontend"));
 app.listen(port,function(){
